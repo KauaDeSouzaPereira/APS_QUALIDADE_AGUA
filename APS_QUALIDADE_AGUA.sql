cb77570171fa31d619c0aa4a4604c82deff77751
Kauã De Souza Pereira <105396156+KauaDeSouzaPereira@users.noreply.github.com>
2024-05-28 00:02:42 -0300

    Delete APS_QUALIDADE_AGUA.sql

--- APS_QUALIDADE_AGUA.sql
+++
@@ -1,7 +0,0 @@
-LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\water_potability.csv"
-IGNORE INTO TABLE sensores
-FIELDS TERMINATED BY ';'
-IGNORE 1 ROWS
-(ph, dureza, solido, sulfato, condutividade , turbidez, potabilidade);
-
-SELECT * FROM aps_qualidade_agua.sensores;
