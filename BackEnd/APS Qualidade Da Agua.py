155eb9225e8394d5fd665b4662cc85989c8d2757
Kauã De Souza Pereira <105396156+KauaDeSouzaPereira@users.noreply.github.com>
2024-05-30 19:13:38 -0300

    Update APS Qualidade Da Agua.py

--- BackEnd/APS Qualidade Da Agua.py
+++ BackEnd/APS Qualidade Da Agua.py
@@ -3,11 +3,10 @@ import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 import mysql.connector
 from keras.optimizers import SGD
-from keras.regularizers import l2
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
@@ -24,7 +23,7 @@ cursor = conexao.cursor()
 
 # Função para consultar a tabela no banco de dados
 def consultar_dados(cursor):
-    cursor.execute("SELECT ph, dureza, turbidez, solido, condutividade, sulfato, potabilidade FROM sensores")
+    cursor.execute("SELECT ph, dureza, solido, cloramina, sulfato, condutividade , carb_organico, trihametanes, turbidez, potabilidade FROM sensores")
     dados = cursor.fetchall()
     return dados
 
@@ -35,11 +34,53 @@ def inserir_resultado(cursor, conexao, resultado):
     cursor.execute(query, valores)
     conexao.commit()
 
+# Função para realizar a conexão do servidor de socket
+# def iniciar_servidor_socket():
+
+    # Novo socket é criado usando a função socket 
+    # O socket.AF_INET é o domínio do endereço do socket
+    # O socket.SOCK_STREAM está indicando que é um socket TCP, orientado a conexão
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Associando a porta do servidor do Banco de Dados
+    # server_address = ('localhost', 3306)
+    # print('Iniciando servidor em {} porta {}'.format(*server_address))
+    # sock.bind(server_address) O método bind associa o socket a esse endereço do servidor
+
+    # Escute por conexões de entrada
+    # sock.listen(1)
+
+    # while True: Mantendo o servidor rodando, permitindo que ele aceite conexões
+        # Aguarde por uma conexão
+        # print('Aguardando por uma conexão')
+        # connection, client_address = sock.accept() Está aguardando uma conexão de entrada
+
+        # try: O servidor tentará receber dados do cliente e responder.
+            # print('Conexão de', client_address)
+
+            # Receba os dados em pequenos pedaços e retransmita-os
+            # while True:
+                # data = connection.recv(16) Recebendo dados do cliente
+                # print('Recebido {!r}'.format(data))
+
+                # if data: Se algum dado for recebido, o servidor imprimirá os dados e enviará uma resposta de volta ao cliente 
+                    # print('Enviando dados de volta ao cliente')
+                    # connection.sendall(data)
+                # else:
+                    # print('Nenhum dado de', client_address)
+                    # break
+
+        # finally:
+            #connection.close() Fechando a conexão com o cliente
+
+
+# iniciar_servidor_socket() Utilizando a função
+
 # Consultar os dados tabela
 dados_completos = consultar_dados(cursor)
 
 # Converter os dados consultados em um DataFrame do pandas
-df_completo = pd.DataFrame(dados_completos, columns=['ph', 'dureza', 'turbidez', 'solido', 'condutividade', 'sulfato', 'potabilidade'])
+df_completo = pd.DataFrame(dados_completos, columns=['ph', 'dureza', 'solido', 'cloramina', 'sulfato', 'condutividade' , 'carb_organico', 'trihametanes', 'turbidez', 'potabilidade'])
 
 df_completo = df_completo.dropna()
 
@@ -51,58 +92,85 @@ y = df_completo['potabilidade'].values
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-# Dividindo os dados em conjuntos de treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 42)
-
 # Definindo o modelo da IA
 model = Sequential()
-model.add(Dense(64, activation='relu', input_shape=(X_train.shape[1],)))
-model.add(Dense(32, activation='relu', input_shape=(X_train.shape[1],)))
-model.add(Dense(1, activation='sigmoid', input_shape=(X_train.shape[1],)))
+model.add(Dense(64, activation='relu', input_shape=(X.shape[1],)))
+model.add(Dense(32, activation='relu',))
+model.add(Dense(1, activation='sigmoid',))
 
 # Compilando o modelo da IA
-opt = SGD(learning_rate=0.01)
-model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
+opt = SGD(learning_rate=0.04)
+model.compile(optimizer=opt, loss='mean_squared_error', metrics=['accuracy'])
 
-# Treinando o modelo nos dados de treino
-history = model.fit(X_train, y_train, epochs=300, batch_size=32, validation_data=(X_test, y_test))
+# Definindo a validação cruzada
+kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
+# Listas para armazenar as métricas de cada fold
+train_acc_histories = []
+test_acc_histories = []
+train_loss_histories = []
+test_loss_histories = []
 
-# Colocando os dados da acurácia dentro de um gráfico de visualização
-plt.figure(figsize=(12, 4))
+# K-fold Cross Validation
+for train, test in kfold.split(X, y):
+    # Treinando o modelo nos dados de treino
+    history = model.fit(X[train], y[train], epochs=60, batch_size=32, validation_data=(X[test], y[test]))
 
-plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'], label='Acurácia de Treino')
-plt.plot(history.history['val_accuracy'], label='Acurácia de Teste')
-plt.title('Acurácia de Treino e Teste')
+    # Armazenando as métricas de treinamento
+    train_acc_histories.append(history.history['accuracy'])
+    test_acc_histories.append(history.history['val_accuracy'])
+    train_loss_histories.append(history.history['loss'])
+    test_loss_histories.append(history.history['val_loss'])
+
+# Calculando a acurácia e a perda média de treino e teste ao longo das épocas
+train_acc_mean = np.mean(train_acc_histories, axis=0)
+test_acc_mean = np.mean(test_acc_histories, axis=0)
+train_loss_mean = np.mean(train_loss_histories, axis=0)
+test_loss_mean = np.mean(test_loss_histories, axis=0)
+
+# Plotando a acurácia média de treino e teste ao longo das épocas
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(train_acc_mean, label='Acurácia de Treino', color='blue')
+plt.title('Acurácia de Treino')
 plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'], label='Perda de Treino')
-plt.plot(history.history['val_loss'], label='Perda de Teste')
-plt.title('Perda de Treino e Teste')
+plt.subplot(2, 2, 2)
+plt.plot(test_acc_mean[::-1], label='Acurácia de Teste', color='orange')  # Invertendo o eixo x
+plt.title('Acurácia de Teste')
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.plot(train_loss_mean, label='Perda de Treino', color='blue')
+plt.title('Perda de Treino')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(test_loss_mean[::-1], label='Perda de Teste', color='orange')  # Invertendo o eixo x
+plt.title('Perda de Teste')
 plt.legend()
 
 plt.tight_layout()
 plt.show()
 
 # Fazendo previsões no conjunto de teste
-y_pred = model.predict(X_test)
+y_pred = model.predict(X[test])
 y_pred_classes = (y_pred > 0.5).astype(int)
 
-# Gerando a matriz de confusão, pra saber quantos a IA acertou ou não
-cm = confusion_matrix(y_test, y_pred_classes)
+# Gerando a matriz de confusão
+cm = confusion_matrix(y[test], y_pred_classes)
 
-# Criando a matriz de confusão
+# Plotando a matriz de confusão
 plt.figure(figsize=(6, 6))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
 plt.title('Matriz de Confusão')
 plt.xlabel('Previsão')
-plt.ylabel('Verdadeiro')
+plt.ylabel('Acerto')
 plt.show()
 
 # Fazendo previsões no conjunto de teste
-previsoes = model.predict(X_test)
+previsoes = model.predict(X[test])
 previsoes_classes = (previsoes > 0.5).astype(int)
 
 # Inserir o resultado no banco de dados
